52cb1032712d630a57c8c8a917aeba6e3ba75871
joaquinjimenezgarcia <garcia.jjimenez@gmail.com>
2017-08-26 13:55:07 +0200

    - Añadidos método y ruta para crear nuevos álbums

--- control/album.js
+++ control/album.js
@@ -12,6 +12,30 @@ function getAlbum(req, res) {
     res.status(200).send({message: 'Acción getAlbum'});
 }
 
+function saveAlbum(req, res) {
+    var album = new Album();
+    var params = req.body;
+
+    album.title = params.title;
+    album.description = paramos.description;
+    album.year = params.year;
+    album.image = 'null';
+    album.artist = params.artist;
+
+    album.save((err, albumStored) => {
+        if (err) {
+            res.status(500).send({message: 'Error en la petición'});
+        } else {
+            if (!albumStored) {
+                res.status(404).send({message: 'No se ha guardado el álbum'});
+            } else {
+                res.status(200).send({album: albumStored});
+            }
+        }
+    });
+}
+
 module.exports = {
-    getAlbum
+    getAlbum,
+    saveAlbum
 };
--- routes/album.js
+++ routes/album.js
@@ -11,6 +11,7 @@ var md_auth = require('../middleware/authenticated');
 var multipart = require('connect-multiparty');
 var md_upload = multipart({uploadDir:'./uploads/album'});
 
-api.get('/album', md_auth.ensureAuth, ArtistController.getAlbum);
+api.get('/album', md_auth.ensureAuth, AlbumController.getAlbum);
+api.post('/album', md_auth.ensureAuth, AlbumController.saveAlbum);
 
 module.exports = api;
