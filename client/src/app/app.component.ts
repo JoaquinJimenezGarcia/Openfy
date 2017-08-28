e2ae6b220a59f6a67dbecbcaaabe132218f74c48
joaquinjimenezgarcia <garcia.jjimenez@gmail.com>
2017-08-28 21:09:50 +0200

    - Creados formulario de registro e identificación

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { User } from './model/user';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'OpenFy';
+  public title = 'OpenFy';
+  public user: User;
+  public identity;
+  public token;
+
+  constructor() {
+    this.user = new User('', '', '', '', '','ROLE_USER', '');
+  }
+
+  public onSubmit() {
+    console.log(this.user);
+  }
 }
